6f26b5b8c912d8ae5d5028a57f8da2af6b44426f
Rossprehn <32393462+Rossprehn@users.noreply.github.com>
2018-03-10 13:56:51 -0700

    deploy

--- migrations/20180310113830_questions.js
+++ migrations/20180310113830_questions.js
@@ -10,5 +10,5 @@ exports.up = function(knex, Promise) {
   })
 }
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists(questions)
+  return knex.schema.dropTableIfExists('questions')
 }
--- queries.js
+++ queries.js
@@ -1,30 +1,30 @@
 const database = require('./database-connection')
 
 module.exports = {
-  list(table) {
-    return database(table).select()
+  list(questions) {
+    return database('questions').select()
   },
-  read(table, id) {
-    return database(table)
+  read(questions, id) {
+    return database('questions')
       .select('*')
       .where('id', id)
       .first()
   },
-  create(table, item) {
-    return database(table)
+  create(questions, item) {
+    return database('questions')
       .insert(item)
       .returning('*')
       .then(record => record[0])
   },
-  update(table, id, question) {
-    return database(table)
+  update(questions, id, question) {
+    return database('questions')
       .update(question)
       .where('id', id)
       .returning('*')
       .then(record => record[0])
   },
-  delete(table, id) {
-    return database(table)
+  delete(questions, id) {
+    return database('questions')
       .delete()
       .where('id', id)
   }
