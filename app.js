ab0ef4292d24aab530adb25c31b717d6c39599d6
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-05-30 15:32:35 -0600

    updating to hopefully get PUT working correctly

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const queries = require('./queries')
-const bodyParser = require('body-parser')
-const database = require('./database-connection')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const queries = require("./queries");
+const bodyParser = require("body-parser");
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cors());
 
-app.get('/questions', (request, response) => {
-  queries.list('questions').then(questions => {
-      response.json({ questions })
-    }).catch(console.error)
+app.get("/questions", (request, response) => {
+  queries.list("questions").then(questions => {
+      response.json({ questions });
+    }).catch(console.error);
 });
 
-app.get('/questions/:id', (request, response) => {
+app.get("/questions/:id", (request, response) => {
   queries.read(request.params.id).then(questions => {
       questions
-      ? response.json({ questions })
-      : response.sendStatus(404)
-    }).catch(console.error)
+      ? response.json({questions})
+      : response.sendStatus(404);
+    }).catch(console.error);
 });
 
-app.post('/questions', (request, response) => {
+app.post("/questions", (request, response) => {
   queries.create(request.body).then(questions => {
-      response.status(201).json({ questions: questions })
-    }).catch(console.error)
+      response.status(201).json({questions: questions});
+    }).catch(console.error);
 });
 
-app.delete('/questions/:id', (request, response) => {
+app.delete("/questions/:id", (request, response) => {
   queries.delete(request.params.id).then(() => {
-      response.sendStatus(204)
-    }).catch(console.error)
+      response.sendStatus(204);
+    }).catch(console.error);
 });
 
-app.put('/questions/:id', (request, response) => {
+app.put("/questions/:id", (request, response) => {
   queries.update(request.params.id, request.body).then(questions => {
-      response.json({ questions: questions[0] })
-    }).catch(console.error)
+      response.json({questions: questions[0]});
+    }).catch(console.error);
 });
 
 app.use((request, response) => {
-  response.send(404)
+  response.send(404);
 });
 
 module.exports = app
